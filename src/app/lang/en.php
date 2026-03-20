f71d00bc4eba1035a8d3d98cfb2b27b52c3978ed
Miguel Angel Martinez <miguelangel.martinez@airenetworks.es>
2026-03-20 14:21:06 +0100

    feat: Nuevo login añadido y la carpeta para controlar los idiomas

---
+++ src/app/lang/en.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'arrayGlobal' => [
+        'app_name' => 'Coffmunnity',
+        'version' => 'Actual Version - 1.0.0'
+    ],
+    'arrayLogin' => [
+        'page_title' => 'Login',
+        'title' => 'Log In',
+        'placeholder_user' => 'Username',
+        'placeholder_pass' => 'Password',
+        'show_pass' => 'Show password',
+        'remember_me' => 'Remember me',
+        'btn_submit' => 'Access',
+        'forgot_pass' => 'Forgot your password?'
+    ]
+];
+?>
