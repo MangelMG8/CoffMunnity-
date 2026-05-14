770b50b65e4198394edf44d026e7db57e9892158
Miguel Angel Martinez <miguelangel.martinez@airenetworks.es>
2026-05-14 16:48:41 +0200

    refactor: New sidebar appareance

--- src/app/includes/footer.php
+++ src/app/includes/footer.php
@@ -4,16 +4,8 @@
       <i class="fa-solid fa-house"></i>
       <span>Inicio</span>
     </a>
-    <a href="/buscar.php" class="bottom-nav-item">
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <span>Buscar</span>
-    </a>
     <a href="/crear.php" class="bottom-nav-item bottom-nav-create" aria-label="Crear publicación">
       <i class="fa-solid fa-plus"></i>
-    </a>
-    <a href="/comunidad.php" class="bottom-nav-item">
-      <i class="fa-solid fa-users"></i>
-      <span>Comunidad</span>
     </a>
     <a href="/perfil.php" class="bottom-nav-item">
       <i class="fa-solid fa-user"></i>
